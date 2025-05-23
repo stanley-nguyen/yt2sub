2b9f838f25012bfdcdca9b898aaadc39c19e6174
stanley-nguyen <stanley.nguyen@sjsu.edu>
2025-05-23 00:05:25 -0700

    add github link and logo

--- client/src/App.vue
+++ client/src/App.vue
@@ -127,6 +127,13 @@ const inputProps = computed(() => {
                      @update:status="(st) => status = st"
                      @update:language="(lg) => sourceLanguage = lg"/>
         </div>
+
+        <a class="links" href="https://github.com/stanley-nguyen/yt2sub" target="_blank">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
+          </svg>
+          <span class="link-text">GitHub</span>
+        </a>
       </div>
 
       <div class="result-container" v-if="transcribed">
@@ -164,6 +171,8 @@ const inputProps = computed(() => {
 
 .input-container {
   padding: 0 5vw;
+  justify-items: center;
+  padding-bottom: 10px;
 }
 
 .result-container {
@@ -191,5 +200,20 @@ header {
 .loading-box {
   filter: drop-shadow(0px 0px 8px rgba(255, 255, 255, 0.7));
   font-size: 14px;
+}
+
+.links {
+  width: 10px;
+  color: #9c9c9c;
+  padding: 4px 5px;
+  border-radius: 10px;
+}
+
+.links:hover {
+  background-color: #9c9c9c33;
+}
+
+.link-text {
+  padding-left: 5px;
 }
 </style>
